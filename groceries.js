9ae8ead10cda208397f54cfe4911506186dd19ea
Deyci Lopez <dlopez-rangel@mail.csuchico.edu>
2016-11-14 22:22:16 -0800

    Changes for the adding of button

--- groceries.js
+++ groceries.js
@@ -3,6 +3,14 @@ function addItem(){
   var list = document.getElementById("ListDisplay");
   var item = document.createElement("li");
   var itemName = document.createTextNode(input);
+  var btnClose = document.createElement("button")
+
+  btnClose.classList.add("btn");
+  btnClose.classList.add("btn-danger");
+  btnClose.classList.add("btn-xs");
+  var iconClose = document.createElement("span");
+  iconClose.classList.add("glyphicon");
+  iconClose.classList.add("glyphicon-remove");
   item.appendChild(itemName);
   list.appendChild(item);
   document.getElementById("newItem").innerHTML = "";
